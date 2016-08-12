59987f4ccfde96ae36ffc7c86d25f1917a30d090
Shradha Pulla <pullashradha@gmail.com>
2016-08-11 21:02:48 -0700

    Create meal-list.component to show manually created meals

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
 import {Component} from "angular2/core";
 import {Meal} from "./meal.model";
+import {MealComponent} from "./meal.component";
+import {MealListComponent} from "./meal-list.component";
 
 @Component ({
   selector: "my-app",
+  directives: [MealListComponent],
   template:
   `
-    <h1>Meal Tracker</h1>
+    <h1>Meal List</h1>
+    <meal-list mealList="meals"></meal-list>
   `
 })
 
 export class AppComponent {
-
+  public meals: Meal[];
+  constructor() {
+    this.meals = [
+      new Meal("Apple Pie", 1000, "It was Dutch apple pie."),
+      new Meal("Cheesecake", 550, "It was delicious, and it's made of milk so it has to be healthy!")
+    ];
+  }
 }
